0c8a9a7bf2c4448616570b4d2fc8b26bdc3f2e49
fiqihrivai-web <fiqih.rivai@gmail.com>
2026-04-26 22:16:20 +0700

    fix: clean package config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
-const nextConfig = {
-  experimental: {
-    turbo: false,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
   },
 };
 
